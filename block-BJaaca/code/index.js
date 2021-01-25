54e716dbbd9bcd2ab4c7aec0f377f74fb955a976
sumit <sumitthapa90@gmail.com>
2021-01-25 14:40:05 +0530

    check

--- block-BJaaca/code/index.js
+++ block-BJaaca/code/index.js
@@ -2,21 +2,34 @@
 
 //  - Create a variable named `character` using `let` assign a value of empty object.
 
+let character = {};
+
 //  - Go to this [Arya Stark](https://awoiaf.westeros.org/index.php/Arya_Stark)
 
 //  Re-assign the value of `character` variable to and object with the key `characterName` and value of "Arya"
 
+character = {
+  characterName: "Arya",
+};
 // - A variable named `age` is predefined with value 20. Add a new key named `character-age` with the value of `age` variable i.e 20 (don't use the value 20)
-
 let age = 20;
+character.characterAge = age;
 
 // - Add a new key `sur name` with the value of the surname of the character i.e "Stark". (Don't remove the space between `sur` and `name`)
+character["sur name"] = "stark";
 
 // - Add a new key `title` with the value title of the character i.e "Lady of Winterfell"
 
+character.title = "lady of winterfall";
+
 // - Add another property named `greet` and value should be a function when called should alert `I am [NAME HERE] and my title is [TITLE HERE]`.
 
+character.greet = function () {
+  alert(`I am ${character.characterName} and my title is ${character.title}`);
+};
+
 // - Add a new property to the character object named `isFemale` and value will be `true`
+    character.isFemale = true;
 
 // - Execute the function stored in the greet key of the character object
 
@@ -30,7 +43,7 @@ let age = 20;
 
 // - Add a new key with the name stored in variable `keyName` defined below i.e `playedBy` (use [] for computed property) and the value of "Maisie Williams"
 
-let keyName = 'playedBy';
+let keyName = "playedBy";
 
 // - Using `alert` alert the value stored in key you added above `keyName` use the variable name to access
 
@@ -39,3 +52,4 @@ let keyName = 'playedBy';
 // - Add a new key named `totalSeasons` with the value of `1 + 2 + 3 + 2`. Use this expression
 
 // - Access the value of the key `totalSeason` (it should be 8)
+
